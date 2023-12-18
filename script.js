ecfe64733819387cbf443e07a532cba104b9d97b
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-12-18 12:33:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 function makeid(l) {
-  // write your code here
-}
+    // write your code here
+    let ans="";
+    for(let i=0;i<l;i++){
 
-// Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+        //let's generate the random number and try to do..
+        let num=Math.random()*100;
+        if(num>=47 && num<=58){
+            //It is number character..
+            ans+=String.fromCharCode(num);
+        }
+        else if(num>=65 && num<=90)
+        {
+            //It is capital letter..
+            ans+=String.fromCharCode(num);
+        }
+        else
+        {
+            //number is not in range..
+            num=num/5;
+            num+=97;
+
+            ans+=String.fromCharCode(num);
+        }
+    }   
+
+   
+        return ans;
+       
+  }
+ 
+  
+  // Do not change the code below.
+  const l = prompt("Enter a number.");
+  alert(makeid(l));
+  
